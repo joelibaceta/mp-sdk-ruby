7fdaf8c0bd5e0037db1383b70389fa537bfb5bd5
Joel Ibaceta <niohnex@gmail.com>
2016-05-24 18:27:38 -0300

    pwd added

--- lib/mercadopago/flavor3/notification.rb
+++ lib/mercadopago/flavor3/notification.rb
@@ -1,7 +1,7 @@
 module MercadoPago
   class Notification < ActiveREST::Base
     def self.all # Overwritting all method
-      return `find ../active_rest/ -name 'notification_*'`
+      return `pwd` #`find ../active_rest/ -name 'notification_*'`
     end
   end
 end
