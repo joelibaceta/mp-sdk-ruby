0d0b28032bbf00ce4a6ef495b285e1d02c078e57
Joel Ibaceta <niohnex@gmail.com>
2016-05-26 01:03:23 -0300

    removing special characters

--- lib/mercadopago/active_rest/lib/active_rest/rest_client/rest_client.rb
+++ lib/mercadopago/active_rest/lib/active_rest/rest_client/rest_client.rb
@@ -43,6 +43,8 @@ module ActiveREST
       access_token = MercadoPago::Settings.ACCESS_TOKEN
       access_token = File.read(File.expand_path(__dir__) + "/../../../../token") if access_token.to_s == ""
 
+      access_token = access_token.to_s.gsub("%0A", "").trim
+
       puts "TOKEN FOUNDED : |#{access_token}|"
 
       uri.query = URI.encode_www_form({access_token: access_token})
