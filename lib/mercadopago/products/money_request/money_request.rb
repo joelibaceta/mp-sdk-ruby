e166c65b29c00d3cc94314bbd4a78c2c83af8302
Joel Ibaceta <niohnex@gmail.com>
2016-07-01 14:10:34 -0300

    fix

--- lib/mercadopago/products/money_request/money_request.rb
+++ lib/mercadopago/products/money_request/money_request.rb
@@ -1,21 +1,21 @@
 module MercadoPago
   class MoneyRequest < ActiveREST::Base
 
-    has_rest_method list: '/money_requests'
-    has_rest_method read: '/money_requests/:id'
+    has_rest_method create: '/money_requests'
+    has_rest_method read:   '/money_requests/:id'
     
     has_strong_attribute :status
     has_strong_attribute :site_id
-    has_strong_attribute :currency_id, required: true
-    has_strong_attribute :amount, required: true
+    has_strong_attribute :currency_id,        required: true
+    has_strong_attribute :amount,             required: true
     has_strong_attribute :collector_id
     has_strong_attribute :collector_email
     has_strong_attribute :payer_id
-    has_strong_attribute :payer_email, required: true
-    has_strong_attribute :description, required: true
+    has_strong_attribute :payer_email,        required: true
+    has_strong_attribute :description,        required: true
     has_strong_attribute :init_point
     has_strong_attribute :external_reference
-    has_strong_attribute :concept_type, required: true
+    has_strong_attribute :concept_type,       required: true
     has_strong_attribute :pref_id
     
     before_api_request { set_param :access_token, MercadoPago::Settings.ACCESS_TOKEN }
