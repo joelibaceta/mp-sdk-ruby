1598f526674d6586b3fcea01bb38b20117ff2a78
Joel Ibaceta <niohnex@gmail.com>
2016-07-01 13:49:17 -0300

    required fields added

--- lib/mercadopago/products/money_request/money_request.rb
+++ lib/mercadopago/products/money_request/money_request.rb
@@ -6,16 +6,16 @@ module MercadoPago
     
     has_strong_attribute :status
     has_strong_attribute :site_id
-    has_strong_attribute :currency_id 
-    has_strong_attribute :amount
+    has_strong_attribute :currency_id, required: true
+    has_strong_attribute :amount, required: true
     has_strong_attribute :collector_id
     has_strong_attribute :collector_email
     has_strong_attribute :payer_id
-    has_strong_attribute :payer_email 
-    has_strong_attribute :description
+    has_strong_attribute :payer_email, required: true
+    has_strong_attribute :description, required: true
     has_strong_attribute :init_point
     has_strong_attribute :external_reference
-    has_strong_attribute :concept_type
+    has_strong_attribute :concept_type, required: true
     has_strong_attribute :pref_id
     
     before_api_request { set_param :access_token, MercadoPago::Settings.ACCESS_TOKEN }
